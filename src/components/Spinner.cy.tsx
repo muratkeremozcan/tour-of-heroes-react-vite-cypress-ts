d0dbf0a6d792b27c8cb2f6cf831d045dae05ad85
Murat Ozcan <muratkerem@gmail.com>
2023-07-07 11:48:55 -0500

    trigger a failure

--- src/components/Spinner.cy.tsx
+++ src/components/Spinner.cy.tsx
@@ -3,6 +3,6 @@ import Spinner from './Spinner'
 describe('Spinner', () => {
   it('should render a spinner', () => {
     cy.mount(<Spinner />)
-    cy.getByCy('spinner').should('be.visible')
+    cy.getByCy('spinner').should('not.be.visible')
   })
 })
